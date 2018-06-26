7148c980bb073841ba3b6b017f83b3928a2fd0f3
samblacklock <samblacklock@me.com>
2018-06-26 23:07:39 +0100

    add styled-jsx, add basic page styles

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { bodyDark, bodyLight } from '../global/styles';
+
+export default () => (
+  <Fragment>
+
+    <style jsx global>
+      {`
+        html, body, #app {
+          height: 100vh
+          margin: 0
+          overflow: hidden
+          padding: 0
+          width: 100vw
+        }
+
+        body {
+          background: linear-gradient(to top, ${bodyDark}, ${bodyLight})
+
+          * {
+            font-family: Avenir, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'
+          }
+        }
+
+        #app {
+          align-items: center
+          display: flex
+          flex-direction: column
+          justify-content: center
+        }
+      `}
+    </style>
+  </Fragment>
+);
---
+++ src/global/styles.js
@@ -0,0 +1,4 @@
+// some basic reusable styles/colours etc
+ 
+export const bodyDark = '#acb6e5';
+export const bodyLight = '#74ebd5';
